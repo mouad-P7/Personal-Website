a3f21bcef2d305189d667e1a8b418bef153c2ab5
mouad-p7 <mouadananouch7@gmail.com>
2023-02-05 03:57:33 +0100

    Add about.html page.

--- src/js/about.js
+++ src/js/about.js
@@ -2,7 +2,8 @@
 import '../scss/style.scss';
 
 // import images
-import "../images/favicon.jpg";
+import '../images/favicon.jpg';
+import '../images/logo-p7.png';
 
 // import js
 import './menu-btn';
