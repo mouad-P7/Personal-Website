e2e9926a3fbd0d74c7eec67b9c5c3dfc6d27d2d4
mouad-p7 <mouadananouch7@gmail.com>
2023-02-06 16:56:24 +0100

    Add style to #skills div, and update #about div style.

--- src/js/about.js
+++ src/js/about.js
@@ -4,6 +4,8 @@ import '../scss/style.scss';
 // import images
 import '../images/favicon.jpg';
 import '../images/logo-p7.png';
+import '../images/jquery-icon.jpg';
+import '../images/webpack-icon.png';
 
 // import js
 import './menu-btn';
