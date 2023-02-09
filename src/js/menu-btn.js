f6650b5476851122bac76074459445c2251c3c76
mouad-p7 <mouadananouch7@gmail.com>
2023-02-09 12:22:20 +0100

    Fix nav-bar button bug.

--- src/js/menu-btn.js
+++ src/js/menu-btn.js
@@ -1,6 +1,22 @@
 $(document).ready(function(){
 
 
+  let mediumWidth = 720;//720px
+  $(window).resize(function(){
+    console.log('resize')
+    let width = $(this).innerWidth();
+    if(width > mediumWidth){
+      $('.header-nav-btn').css({'display':'none'});
+    }else{
+      if($('nav').css('left') == 0){
+        $('.close-menu-btn').css({'display':'inline-block'});
+      }else{
+        $('.open-menu-btn').css({'display':'inline-block'});
+      }
+    }
+  });
+
+
   $('.open-menu-btn').click(function(){
     $('.open-menu-btn').css({'display':'none'})
     $('nav').css({'left':'0'})
