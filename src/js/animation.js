f1529863fc16f366bad8e927480f62a1b385e2c7
mouad-p7 <mouadananouch7@gmail.com>
2023-03-31 22:28:57 +0000

    Add basic gsap animation + img and loader update.

--- src/js/animation.js
+++ src/js/animation.js
@@ -1,7 +1,39 @@
-// gsap code
-gsap.to('code',{
-  duration: 5, 
-  delay: 1,
-  color: 'red',
-  // transform: rotate3d(1, 0, 0, 90),
-});
+window.onload = function() {
+  // gsap code
+  gsap.fromTo('code',{
+    y: '-100%',
+    opacity: 0,
+  },{
+    y: 0,
+    opacity: 1,
+    ease: 'power2',
+    duration: 1, 
+    delay: 1,
+  });
+
+
+  gsap.fromTo('h1, h2',{
+    x: '-50%',
+    opacity: 0,
+  },{
+    x: 0,
+    opacity: 1,
+    ease: 'power2',
+    duration: 1, 
+    delay: 1.3,
+  });
+
+
+  gsap.fromTo('p, button, .certif, .skill, .info',{
+    x: '-50%',
+    opacity: 0,
+  },{
+    x: 0,
+    opacity: 1,
+    ease: 'power2',
+    duration: 1, 
+    delay: 1.6,
+  });
+};
+
+
